7fd005b452d52757fb8714d64a8299b0461c1fbe
Amirah S Brown <amirahbrown@storm.cis.fordham.edu>
2016-02-16 00:35:45 -0500

    My commit

---
+++ HelloWorld.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string amirahbrown;
+  cout << amirahbrown
+  cin >> amirahbrown;
+  cout << "Hello " << amirahbrown  << endl;
+       << Nice to meet you! << endl;
+  return 0;
+}:w
+:w
+
